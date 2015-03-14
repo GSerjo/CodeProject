fc4c48cea81805580faa2d0473a4faf3d28b7572
GSerjo <sedot8@gmail.com>
2015-03-14 22:15:27 +0300

    + TinyMapper samples

---
+++ TinyMapper/Source/Samples/Contracts/PersonDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Samples.Contracts
+{
+    public sealed class PersonDto
+    {
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public Guid Id { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ TinyMapper/Source/Samples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Nelibur.ObjectMapper;
+using Samples.Contracts;
+
+namespace Samples
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+
+private static void MapPerson()
+{
+    TinyMapper.Bind<Person, PersonDto>();
+
+    var person = new Person
+    {
+        Id = Guid.NewGuid(),
+        FirstName = "John",
+        LastName = "Doe",
+        Email = "support@tinymapper.net",
+        Address = "Wall Street"
+    };
+
+    var personDto = TinyMapper.Map<PersonDto>(person);
+}
+    }
+}
