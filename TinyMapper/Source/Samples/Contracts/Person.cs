9d83a8185dc3d3249ee9098e2af23654df292be5
GSerjo <sedot8@gmail.com>
2015-03-15 12:41:17 +0300

    * Program added Measure for TinyMapper, AutoMapper and Handwritten code

--- TinyMapper/Source/Samples/Contracts/Person.cs
+++ TinyMapper/Source/Samples/Contracts/Person.cs
@@ -5,9 +5,12 @@ namespace Samples.Contracts
     public sealed class Person
     {
         public string Address { get; set; }
+        public DateTime CreateTime { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public Guid Id { get; set; }
         public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Phone { get; set; }
     }
 }
--- TinyMapper/Source/Samples/Program.cs
+++ TinyMapper/Source/Samples/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using AutoMapper;
 using Nelibur.ObjectMapper;
 using Samples.Contracts;
 
@@ -6,24 +8,110 @@ namespace Samples
 {
     internal class Program
     {
-        private static void Main()
+        private const int Iterations = 1000000;
+
+        private static Person CreatePerson()
         {
+            return new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "support@tinymapper.net",
+                Address = "Wall Street",
+                CreateTime = DateTime.Now,
+                Nickname = "Object Mapper",
+                Phone = "Call Me Maybe "
+            };
         }
 
-private static void MapPerson()
-{
-    TinyMapper.Bind<Person, PersonDto>();
+        private static void Main()
+        {
+            MeasureHandwritten();
+            MeasureTinyMapper();
+            MeasureAutoMapper();
 
-    var person = new Person
-    {
-        Id = Guid.NewGuid(),
-        FirstName = "John",
-        LastName = "Doe",
-        Email = "support@tinymapper.net",
-        Address = "Wall Street"
-    };
+            Console.ReadKey();
+        }
 
-    var personDto = TinyMapper.Map<PersonDto>(person);
-}
+        private static PersonDto MapHandwritten(Person person)
+        {
+            var result = new PersonDto
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                Email = person.Email,
+                Address = person.Address,
+                CreateTime = person.CreateTime,
+                Nickname = person.Nickname,
+                Phone = person.Phone
+            };
+            return result;
+        }
+
+        private static void MapPerson()
+        {
+            TinyMapper.Bind<Person, PersonDto>();
+
+            var person = new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "support@tinymapper.net",
+                Address = "Wall Street",
+                CreateTime = DateTime.Now,
+                Nickname = "Object Mapper",
+                Phone = "Call Me Maybe "
+            };
+
+            var personDto = TinyMapper.Map<PersonDto>(person);
+        }
+
+        private static void MeasureAutoMapper()
+        {
+            Person person = CreatePerson();
+            Mapper.CreateMap<Person, PersonDto>();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                var personDto = Mapper.Map<PersonDto>(person);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("AutoMapper: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+        }
+
+        private static void MeasureHandwritten()
+        {
+            Person person = CreatePerson();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                PersonDto personDto = MapHandwritten(person);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Handwritten: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+        }
+
+        private static void MeasureTinyMapper()
+        {
+            Person person = CreatePerson();
+            TinyMapper.Bind<Person, PersonDto>();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                var personDto = TinyMapper.Map<PersonDto>(person);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("TinyMapper: {0}ms", stopwatch.Elapsed.TotalMilliseconds);
+        }
     }
 }
